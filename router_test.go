2b81403ebe19407224689d25390971f2a9cf9827
David Zhu <shaowenzhu@dal.ca>
2024-04-28 22:25:44 -0300

    test: add unit test on router

--- router_test.go
+++ router_test.go
@@ -1,11 +1,63 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
 func newTestRouter() *router {
 	r := newRouter()
 	r.addRoute("GET", "/", nil)
-	r.addRoute("GET", "/hello/:name", nil)
+	r.addRoute("GET", "/hello/:id/chat", nil)
 	r.addRoute("GET", "/hello/b/c", nil)
-	r.addRoute("GET", "/hi/:name", nil)
+	r.addRoute("GET", "/hi/:id/chat", nil)
 	r.addRoute("GET", "/assets/*filepath", nil)
 	return r
+}
+
+func TestParsePattern(t *testing.T) {
+	ok := reflect.DeepEqual(parsePattern("/api/chat/v1"), []string{"api", "chat", "v1"})
+	ok = ok && reflect.DeepEqual(parsePattern("api/chat/"), []string{"api", "chat"})
+	ok = ok && reflect.DeepEqual(parsePattern("/api//chat/"), []string{"api", "chat"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/:name"), []string{"p", ":name"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*"), []string{"p", "*"})
+	ok = ok && reflect.DeepEqual(parsePattern("/p/*name/*"), []string{"p", "*name"})
+	if !ok {
+		t.Fatal("test parsePattern failed")
+	}
+}
+
+func TestGetRoute(t *testing.T) {
+	r := newTestRouter()
+	n, ps := r.getRoute("GET", "/hello/003/chat")
+
+	if n == nil {
+		t.Fatal("nil shouldn't be returned")
+	}
+
+	if n.pattern != "/hello/:id/chat" {
+		t.Fatal("should match /hello/:id/chat")
+	}
+
+	if ps["id"] != "003" {
+		t.Fatal("name should be equal to '003'")
+	}
+
+	fmt.Printf("matched path: %s, params['id']: %s\n", n.pattern, ps["id"])
+}
+
+func TestGetRoute2(t *testing.T) {
+	r := newTestRouter()
+	n1, ps1 := r.getRoute("GET", "/assets/file1.txt")
+	ok1 := n1.pattern == "/assets/*filepath" && ps1["filepath"] == "file1.txt"
+	if !ok1 {
+		t.Fatal("pattern shoule be /assets/*filepath & filepath shoule be file1.txt")
+	}
+
+	n2, ps2 := r.getRoute("GET", "/assets/css/test.css")
+	ok2 := n2.pattern == "/assets/*filepath" && ps2["filepath"] == "css/test.css"
+	if !ok2 {
+		t.Fatal("pattern shoule be /assets/*filepath & filepath shoule be css/test.css")
+	}
 }
